74f6c3252371a657b2211251ec27a794fd0ee67d
giannisrou <73931515+giannisrou@users.noreply.github.com>
2022-06-05 21:24:21 +0300

    Update indexes_.sql

--- SQL_Code./indexes_.sql
+++ SQL_Code./indexes_.sql
@@ -1,12 +1,9 @@
 CREATE INDEX idx_researcher_id ON WorksOn(researcher_ID);
 CREATE INDEX idx_project_supervisor_id ON Project(supervisor_ID);
-CREATE INDEX idx_project_evaluator_id ON Project(evaluator_ID);
 CREATE INDEX idx_project_executive_id ON Project(exec_ID);
-
 CREATE INDEX idx_project_org_name ON Project(org_name);
 CREATE INDEX idx_res_org_name ON Researcher(org_name);
 CREATE INDEX idx_project_title ON WorksOn(project_title);
 CREATE INDEX idx_Field_project_title ON Project_Field(project_title);
 CREATE INDEX idx_Deliverable_project_title ON Deliverable(project_title);
 CREATE INDEX idx_start_date ON Project(start_date);
-
