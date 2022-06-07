8695ee1ab2e7c5545ba7a82c281ac9c79a6df556
aggep <101709230+aggep@users.noreply.github.com>
2022-06-07 04:38:26 +0300

    Update and rename DataBaseForDummmies_.sql to Create_Tables.sql

--- SQL_Code./Create_Tables.sql
+++ SQL_Code./Create_Tables.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jun 02, 2022 at 11:34 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
