bedbd9b50d304630223672ed633ffc9eec4aedf0
giannisrou <73931515+giannisrou@users.noreply.github.com>
2022-06-07 09:49:12 +0300

    Delete DB_Web_Development. directory

--- DB_Web_Development./controllers/layout.js
+++
@@ -1,7 +0,0 @@
-const { pool } = require('../utils/database');
-
-exports.getLanding = (reg, res, next) => {
-        res.render('landing.ejs', {
-            pageTitle: "Landing Page"
-        })
-} 
